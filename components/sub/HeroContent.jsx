0afdad118cfd737548f4d561cbc37438fc6585b5
Rohannagrut <rgnagrut_b20@ee.vjti.ac.in>
2024-01-21 13:14:25 +0530

    first commit

---
+++ components/sub/HeroContent.jsx
@@ -0,0 +1,18 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+const HeroContent = () => {
+  return (
+    <motion.div
+      initial="Hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+    >
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        <motion.div variants={} className="Welcome-box py-[15px] px-[4px] border border-[#7042f88b] opacity-[0.9]"></motion.div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default HeroContent;
